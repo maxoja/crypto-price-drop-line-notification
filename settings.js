86a53e9cad43e3bab07c29107ebf6a00318be329
Tawan Thampipattanakul <tawan.tpptnk@gmail.com>
2020-10-29 11:50:10 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ module.exports = {
 	COINS: {
 		BTC: {
 			id: 'bitcoin',
-			DROP_THRESH: 0.96, // 0.95
+			DROP_THRESH: 0.95,
 			RSI_THRESH: 35, //55
 			RSI_PERIOD: 14
 		},
 		ETH: {
 			id: 'ethereum',
-			DROP_THRESH: 0.96, // 0.95
+			DROP_THRESH: 0.95,
 			RSI_THRESH: 35, //55
 			RSI_PERIOD: 14
 		},
 		XRP: {
 			id: 'ripple',
-			DROP_THRESH: 0.96, // 0.95
+			DROP_THRESH: 0.95,
 			RSI_THRESH: 35, //55
 			RSI_PERIOD: 14
 		}
